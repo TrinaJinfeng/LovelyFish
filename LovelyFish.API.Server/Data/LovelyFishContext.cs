619a82893eed82effce354c047a2cdbabb4827f6
Jinfeng-Zhang-1158659 <Jinfeng.zhang@lincolnuni.ac.nz>
2025-07-23 12:32:46 +1200

    Add project files

---
+++ LovelyFish.API.Server/Data/LovelyFishContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LovelyFish.API.Models;
+
+
+namespace LovelyFish.API.Data
+{
+    public class LovelyFishContext : DbContext
+    {
+        public LovelyFishContext(DbContextOptions<LovelyFishContext> options)
+        : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LovelyFish.API.Server/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using LovelyFish.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LovelyFish.API.Server
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<LovelyFishContext>
+    {
+        public LovelyFishContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<LovelyFishContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new LovelyFishContext(optionsBuilder.Options);
+        }
+    }
+}
